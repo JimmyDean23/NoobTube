2b461783b72e9c0404ff5c816890a73913ae32da
Billy Dixon <bidixon@mymail.mines.edu>
2013-03-21 23:37:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoobTube::Application.config.secret_token = 'f769348aab89bf617a8d500c17aef7531611936e1dcd4b7df1c2b22339ad00fd49ccf86797185a2e07b86d43f2c2306e2eef4552d13ac06b294257e5efae8e42'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NoobTube::Application.config.session_store :cookie_store, key: '_NoobTube_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NoobTube::Application.config.session_store :active_record_store
